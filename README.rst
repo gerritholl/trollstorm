37e15724cf31014b32de92e658419ce14d57cec7
Gerrit Holl <gerrit.holl@dwd.de>
2021-07-07 17:41:28 +0200

    Set up pyproject.toml, tests, docs

--- README.rst
+++ README.rst
@@ -1,2 +1,4 @@
-# trollstorm
+trollstorm
+----------
+
 Searching for storms in satellite data, at present and in the future
---
+++ setup.py
@@ -0,0 +1,8 @@
+"""Setup using ppsetuptools.
+
+Not sure why this is necessary, but set things up here.
+"""
+
+from ppsetuptools import setup
+
+setup()
